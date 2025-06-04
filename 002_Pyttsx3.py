3017dcf68e9def4de8ef24ae3f50d5f1047741c2
Chetan <chetanx777@gmail.com>
2025-06-04 17:31:56 +0530

    updated 002

--- 002_Pyttsx3.py
+++ 002_Pyttsx3.py
@@ -2,6 +2,6 @@ import pyttsx3
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id) #fem voice
-engine.setProperty('rate', 95) #speech speed
+engine.setProperty('rate', 130) #speech speed
 engine.say("I love chainsaw man.")
 engine.runAndWait()
