c90cd86889d854c6b22f091c838364d5231a7606
Chetan <chetanx777@gmail.com>
2025-06-17 10:51:46 +0530

    Program to display greates of 4 numbers

--- _011_greatest_of_4_number.py
+++ _011_greatest_of_4_number.py
@@ -3,5 +3,7 @@ print("Enter 4 numbers:")
 for i in range(4):
     num_lst.append(int(input("")))
 
-maxnum = max(num_lst)
-print("The greatest number is", maxnum)
+if (num_lst[0] == num_lst[1] == num_lst[2] == num_lst[3]):
+    print("The numbers are equal")
+else:
+    print("The greatest number is", maxnum)
