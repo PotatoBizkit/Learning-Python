e58391fd689e13ae7e987a5cd6f44e83090db39b
Chetan <chetanx777@gmail.com>
2025-06-20 11:20:30 +0530

    Program to print triangle pattern

--- _019_pattern.py
+++ _019_pattern.py
@@ -4,8 +4,17 @@
 #   *****
 
 def triangle(rows):
+    """
+    Prints a equilateral triangle pattern using asterisks.
+
+    Parameters:
+    rows (int): The number of rows in the triangle.
+
+    Returns:
+    None
+    """
     for i in range(1, rows+1):
-        print(" "*(rows-i), end='')
+        print(" "*(rows-i), end='') # comment out this line to print lower triangular pattern
         for j in range(1, 2*i):
             print("*", end='')
         print()
